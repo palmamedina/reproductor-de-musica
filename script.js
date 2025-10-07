1215f6acb39591385d309f68708034c3d8e7efc1
Brandon Palma <luisbrandonpalmamedina46@gmail.com>
2025-10-07 13:39:18 -0600

    [UPDATE] Aquí agregamos un objeto al arreglo allSongs que representa una canción con su ID, título, artista, duración y la ruta del archivo de audio.

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ const playButton = document.getElementById('play');
 const pauseButton = document.getElementById('pause');
 const nextButton = document.getElementById('next');
 const previousButton = document.getElementById('previous');
-const allSongs = [];
+const allSongs = [
+  {
+    id: 0,
+    title: "Scratching The Surface",
+    artist: "Quincy Larson",
+    duration: "4:25",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3"
+  }
+]
