e85e3ee79f8304e45a5b5c94ffeca05a3e39b3bd
Brandon Palma <luisbrandonpalmamedina46@gmail.com>
2025-10-08 09:25:30 -0600

    [UPDATE] Antes de reproducir una canción, debemos asegurarnos de que comience desde el inicio.Si no hay ninguna canción actualmente en reproducción (userData.currentSong es falso o null),establecemos audio.currentTime = 0 para reiniciar el tiempo de reproducción.

--- script.js
+++ script.js
@@ -85,4 +85,8 @@ function playSong(id) {
   const song = userData.songs.find((song) => song.id === id);
   audio.src = song.src;
   audio.title = song.title;
+  
+  if (!userData.currentSong) {
+    audio.currentTime = 0;
+  }
 }
